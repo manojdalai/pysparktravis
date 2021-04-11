48373bb24e55b9feb1ec03df6a8fb23b0397622d
manojdalai <manoj.uce10@gmail.com>
2021-04-12 01:13:18 +0200

    - Generic function to rename column - load data set 2 and remove credit card information - column rename - write data to client_data folder

--- userdefinedfunction.py
+++ userdefinedfunction.py
@@ -2,6 +2,8 @@ from pyspark.sql.functions import udf
 from pyspark.sql.types import BooleanType
 
 class functions:
+
+    # GENERIC FUNCTION TO FILTER DATA FRAME
     def isCountryMatched(countries, cntry):
         for country in countries.split(','):
             if cntry == country:
@@ -10,3 +12,11 @@ class functions:
 
     isCountryMatchedUDF = udf(isCountryMatched, BooleanType())
 
+    # GENERIC FUNCTION TO RENAME COLUMNS
+    def remane_column(columndict, colnames ):
+        for key, value in columndict.items():
+            oldcol, newcol = key, value
+            for i, v in enumerate(colnames):
+                if oldcol in v:
+                    colnames[i] = v.replace(oldcol, newcol)
+        return colnames
